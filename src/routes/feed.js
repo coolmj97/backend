e3d909987f6153f8e1f63a9f2b3aa130a3d94f72
Min Jeong Kim <minnjeong.kim@gmail.com>
2023-10-31 17:09:41 +0900

    ✨ feed 좋아요 기능 추가

--- src/routes/feed.js
+++ src/routes/feed.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const feed = require("../controller/feed");
 
 //피드 목록 조회
-router.get("/list", feed.findAllFeed);
+router.get("/", feed.findAllFeed);
 
 //피드 생성
 router.post("/", feed.createFeed);
@@ -14,6 +14,6 @@ router.patch("/:id", feed.updateFeed);
 router.delete("/:id", feed.deleteFeed);
 
 //좋아요
-// feedRouter.patch("/feed/:feedid/likes", feed.updateLikes);
+router.patch("/:id/likes", feed.updateLikes);
 
 module.exports = router;
