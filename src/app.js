4dfbf0ce486dd3cd8c0a649fd1a09eda2367952e
Min Jeong Kim <minnjeong.kim@gmail.com>
2023-12-15 17:47:13 +0900

    cors error

--- src/app.js
+++ src/app.js
@@ -20,12 +20,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors({ origin: true }));
 
 app.use(bodyParser.json()); // JSON 파싱 설정
 app.use(bodyParser.urlencoded({ extended: true })); // URL-encoded 데이터 파싱 설정
