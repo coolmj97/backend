c945d9e0d362d5567b86072d992357e1608e994e
Min Jeong Kim <minnjeong.kim@gmail.com>
2023-12-15 16:24:09 +0900

    deploy by firebase

--- src/app.js
+++ src/app.js
@@ -19,7 +19,13 @@ admin.initializeApp({
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'snapshot-front.vercel.app'); // update to match the domain you will make the request from
+  next();
+});
+
+// app.use(cors(corsOptions));
 
 app.use(bodyParser.json()); // JSON 파싱 설정
 app.use(bodyParser.urlencoded({ extended: true })); // URL-encoded 데이터 파싱 설정
